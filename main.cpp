d1388d765a9b5fdb64233975630e3da86b519cb7
Nakayama Daiju <42.daiju@gmail.com>
2023-11-01 23:25:28 +0900

    遷移行列の第二固有値が出ている

--- main.cpp
+++ main.cpp
@@ -3,28 +3,65 @@
 #include <Spectra/GenEigsSolver.h>
 #include <Spectra/MatOp/SparseGenMatProd.h>
 #include <iostream>
+#include <cmath>
 
 using namespace Spectra;
-
 using T = Eigen::Triplet<double>;
 
-int main() {
-    // A band matrix with 1 on the main diagonal, 2 on the below-main subdiagonal,
-    // and 3 on the above-main subdiagonal
-    const int n = 10;
-    std::vector<T> triplets;
-    for (int i = 0; i < n; i++) {
-        triplets.emplace_back(i, i, 1.0);
-        if (i > 0) {
-            triplets.emplace_back(i - 1, i, 3.0);
-        }
-        if (i < n - 1) {
-            triplets.emplace_back(i + 1, i, 2.0);
-        }
+const int N = 3;
+
+int calc_local_energy(uint64_t spin, int pos) {
+    int row = pos / N;
+    int col = pos % N;
+    int energy = -2;
+    if (spin & (1 << (row * N + (col + 1) % N))) {
+        energy += 1;
     }
-    Eigen::SparseMatrix<double> M(n, n);
+    if (spin & (1 << (row * N + (col + N + 1) % N))) {
+        energy += 1;
+    }
+    if (spin & (1 << ((row + 1) % N * N + col))) {
+        energy += 1;
+    }
+    if (spin & (1 << ((row + N + 1) % N * N + col))) {
+        energy += 1;
+    }
+    return energy;
+}
+
+/// spin=1,-1
+double flip_prob_metropolis(double beta, int local_energy, int spin) {
+    int diff = local_energy * spin * 2;
+    if (diff < 0) {
+        return 1.0;
+    } else {
+        return std::exp(static_cast<double>(-diff) * beta);
+    }
+}
+
+void gen_uniform_random_choice_flip_prob(double beta, uint64_t spin, std::vector<T> &triplets,
+                                         const std::function<double(double, int, int)>& flip_prob) {
+    double stay_prob = 0.0;
+    for (int pos = 0; pos < N * N; pos++) {
+        auto flipped_spin = spin ^ (1 << pos);
+        auto local_energy = calc_local_energy(spin, pos);
+        auto p = flip_prob(beta, local_energy, (spin & (1 << pos)) ? 1 : -1);
+        triplets.emplace_back(spin, flipped_spin, p / static_cast<double>(N * N));
+        stay_prob += 1.0 - p;
+    }
+    triplets.emplace_back(spin, spin, stay_prob / static_cast<double>(N * N));
+}
+
+int main() {
+    const double beta = 1.0;
+    const uint64_t matrix_size = 1 << (N * N);
+    std::vector<T> triplets;
+    for (uint64_t spin = 0; spin < matrix_size; spin++) {
+        gen_uniform_random_choice_flip_prob(beta, spin, triplets, flip_prob_metropolis);
+    }
+
+    Eigen::SparseMatrix<double> M(matrix_size, matrix_size);
     M.insertFromTriplets(triplets.begin(), triplets.end());
-    std::cout << M << std::endl;
 
     // Construct matrix operation object using the wrapper class SparseGenMatProd
     SparseGenMatProd<double> op(M);
